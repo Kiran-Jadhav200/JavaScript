63fcdecb3b0fb32527a0886ad4109ffb469bd672
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-01-31 16:18:01 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use Strict"; // treat all JS code as newer version
+
+// alert(3+5); // we are using node.js not browser
+
+console.log(3
+    +4) // code readability  should be high
+
+console.log("Kiran")
+
+let name ="kiran"
+let age = 3;
+let isLoggedIn = false;
+let state;
+
+
+// number => 2 to power 53
+//bitInt 
+//string => ""
+//boolean => true or false
+// null => standalone value
+//undefined =>
+// symbol=> unique
+
+
+
+// Object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
